69491b12520170894e37c67693f67393913ca220
kissdgxd <ngigi.26242@students.kyu.ac.ke>
2025-09-19 11:07:20 +0300

    Add files via upload

---
+++ unit.c
@@ -0,0 +1,20 @@
+/*
+Name:George Njau Ngigi
+Reg:CT100/G/26242/25
+DES:LIST OF IT UNITS SEMISTER ONE
+*/
+
+ #include<stdio.h>
+
+int main()//pre-processor directive scanf ()
+{
+    printf("unit\t \tDescription\n");
+    printf("SPH2107\t \tAnalogue Electronic\n");
+    printf("SPI2102\t \tIntroduction To programming and Algorithms\n");
+    printf("SPI2102\t \tComputer Systems And Organisation\n ");
+    printf("SPI2103\t \tIntroduction To Computer Application\n");
+    printf("SPI2120\t \tEssentials Of Accounting\n");
+    printf("SPM2104\t \tMathematics For Science\n");
+    printf("UCU2105\t \tFundamentals Of ICT\n ");
+    printf("UCU2106\t \tCommunication And Information Literacy");
+}
